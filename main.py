be356eabbe9f87c1dbabd18cc71d451c14858205
algebraity <objective.axioma@gmail.com>
2026-01-21 12:21:18 -0500

    refactored code to output to CSV periodically and clear memory for longer computations--successls

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 from plot_invariants import *
 from track_fu_spikes import *
 
+JOBS = mp.cpu_count()
+
 # Compute the invariants of the number field K.
 def compute_field(K):
     d = K.absolute_generator()**2                   # Compute the value of d (K = Q(sqrt(d)))
@@ -23,22 +25,20 @@ def _worker(i):
     return (i, compute_field(K))
 
 # Compute the field invariants of the first n real quadratic fields
-def compute_invariants(n, jobs=None):
+def compute_invariants(nums, jobs=JOBS):
     if jobs is None:
         jobs = mp.cpu_count()
-
+    
+    n = nums[1]
     orig_n = n
     invariants = {}
-    i = 2
+    i = nums[0]
     ds = []
     while n > 0:
-        if is_squarefree(i):
+        if is_squarefree(i) and i != 1:
             ds.append(i)
         n -= 1
         i += 1
-
-        if n-1 > 0 & (i % 10 == 0):
-            print(str(round(((orig_n-n) / (orig_n))*100, 2)) + "%")
 
     try:
         ctx = mp.get_context("fork")
@@ -46,32 +46,45 @@ def compute_invariants(n, jobs=None):
         ctx = mp.get_context()
 
     with ctx.Pool(processes=jobs) as pool:
-        for d, inv in pool.imap_unordered(_worker, ds, chunksize=10000):
+        for d, inv in pool.imap_unordered(_worker, ds, chunksize=1000):
             x = polygen(ZZ, 'x')
             K = NumberField(x**Integer(2) - d, names=('u',))
             invariants[K] = inv
 
     return invariants
 
-def main(n=-1):
+def main(n=-1, batch_size=10000):
     if n == -1:
         n = int(input("Enter the number of fields to compare: "))
         while not (isinstance(n, int)) and not (n > 0):
             n = int(input("Input not accepted. Enter 0 for real and 1 for imaginary: "))
 
-    invariants = compute_invariants(n)
-    if n < 10000:
-        plot_invariants(invariants)
-    inv_spikes = plot_fu_spikes(invariants)
+    invl = JOBS*batch_size
+    rng = []
+    while n % invl != 0:
+        n += 1
+    num_invl = n // invl
+    for i in range(0, num_invl+1):
+        rng.append(invl * i)
 
-    to_csv(invariants, inv_spikes, 'invariants_data.csv', "fu_spikes.csv")
+    for i in range(0, num_invl):
+        invariants = compute_invariants([rng[i], rng[i+1]])
+        csv_name = "invariants_data_" + str(i+1) + ".csv"
+        to_csv(invariants, csv_name)
+        del(invariants)
+        print("Batch " + str(i+1) + "/" + str(num_invl) + " done")
 
-def to_csv(invariants, inv_spikes, datafile, spikesfile):
+    print("All computations have completed.")
+    #if n < 10000:
+    #    plot_invariants(invariants)
+    #inv_spikes = plot_fu_spikes(invariants)
+
+def to_csv(invariants, datafile):
     to_store = [["d", "dK", "hK", "fu", "rK", "mb"]]
     for field in invariants:
         to_store.append(invariants[field])
     np.savetxt(datafile, to_store, delimiter=',', fmt="%s")
-    np.savetxt(spikesfile, [inv_spikes[0], inv_spikes[1]], delimiter=',', fmt="%s")
+    #np.savetxt(spikesfile, [inv_spikes[0], inv_spikes[1]], delimiter=',', fmt="%s")
 
 
 #def to_json(invariants, inv_spikes, filename):
